d120dfc8126c6a96fc85ea8bd1b5a07da3b136db
anandewaka <142850526+anandewaka@users.noreply.github.com>
2024-01-22 19:58:11 -0300

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,19 @@
+
+//ADICIONE OS LINKS DO SEU APP FIREBASE
+const firebaseConfig = {
+   apiKey: "AIzaSyC3GqGHDQ74smcg32eMhdYnXzPkILgA7ug",
+   authDomain: "kiwitiwer.firebaseapp.com",
+   databaseURL: "https://kiwitiwer-default-rtdb.firebaseio.com",
+   projectId: "kiwitiwer",
+   storageBucket: "kiwitiwer.appspot.com",
+   messagingSenderId: "781624333861",
+   appId: "1:781624333861:web:4b61068ef95c68838811cc"
+ };
+
+
+// Inicializar Firebase
+   firebase.initializeApp(firebaseConfig);
+
+
+
+// Adicionar a função para adicionar usuário 
